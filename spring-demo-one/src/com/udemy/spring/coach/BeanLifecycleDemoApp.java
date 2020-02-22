6558827fab8a9f6cd8340d69555c26e954823a35
rajatsh87 <36112987+rajatsh87@users.noreply.github.com>
2020-02-23 00:21:56 +0530

    bean life cycle using anotattions

---
+++ spring-demo-one/src/com/udemy/spring/coach/BeanLifecycleDemoApp.java
@@ -0,0 +1,28 @@
+package com.udemy.spring.coach;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifecycleDemoApp {
+	
+	public static void main(String[] args) {
+		
+		//load spring config file
+		
+		ClassPathXmlApplicationContext context=
+				new ClassPathXmlApplicationContext("beanLifecycle-applicationContext.xml");
+		
+		//retrieve Bean from spring Container
+		
+		Coach theCoach=context.getBean("myCoach",Coach.class);
+		
+		Coach alphaCoach=context.getBean("myCoach",Coach.class);
+		
+		boolean result=(theCoach==alphaCoach);
+		
+		System.out.println("are both beans pointing to same object: "+result);
+		System.out.println("\nmemory loaction of theCoach"+theCoach);
+		System.out.println("\nmemory loaction of alphaCoach"+alphaCoach);
+		
+		context.close();
+	}
+}
--- spring-demo-one/src/com/udemy/spring/coach/FifaCoach.java
+++ spring-demo-one/src/com/udemy/spring/coach/FifaCoach.java
@@ -16,4 +16,14 @@ public String getDailyWorkout() {
 public String getDailyFortune() {
 	return forService.getFortune();
 }
+	//add an init method
+	public void onInitalization() {
+		System.out.println("this method will run after beans are initialized");
+	}
+	
+	//add a destory method
+	
+	public void onDestory() {
+		System.out.println("	perform cleaning stuff");
+	}
 }
