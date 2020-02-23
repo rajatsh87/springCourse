293d4a5fd41ef4fd9accb148580cdabc17010295
rajatsh87 <36112987+rajatsh87@users.noreply.github.com>
2020-02-23 12:51:17 +0530

    method/field auto wireing

--- spring-demo-one/src/com/udemy/spring/annotations/TennisCoach.java
+++ spring-demo-one/src/com/udemy/spring/annotations/TennisCoach.java
@@ -5,12 +5,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	//field Injection
+	@Autowired
 	private FortuneService forService;
+	/*
+	//auto wireing through constructor
 
 	@Autowired
 	public TennisCoach(FortuneService forService) {
 		this.forService = forService;
 	}
+	*/
+	
+	//auto wireing through setter methods (any method name can work)
+//	@Autowired
+//	public void setForService(FortuneService forService) {
+//		this.forService = forService;
+//	}
+	
 
 	@Override
 	public String getDailyWorkout() {
@@ -18,6 +30,7 @@ public class TennisCoach implements Coach {
 		return "Practice you backhand";
 	}
 	
+
 	public String getDailyFortune() {
 		return forService.dailyFortuneService();
 	}
