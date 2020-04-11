b06013a61a67b6298b38f81395343b70e01a44f6
rajatsh87 <36112987+rajatsh87@users.noreply.github.com>
2020-04-11 18:42:29 +0530

    minor changes

--- hb-01-one-to-one-bi/src/com/luv2code/hibernate/demo/CreateInstructorDetailDemo.java
+++ hb-01-one-to-one-bi/src/com/luv2code/hibernate/demo/CreateInstructorDetailDemo.java
@@ -7,7 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
-public class GetInstructorDetailDemo {
+public class CreateInstructorDetailDemo {
 	public static void main(String[] args) {
 		SessionFactory factory=new Configuration()
 						.configure("hibernate.cfg.xml")
